b66a3b8c463f2bc95aa76ea215e645aefd3479b6
balakr <Bala.Krishnancoil@originenergy.com.au>
2020-10-29 22:38:09 +1100

    first commit

--- src/score.js
+++ src/score.js
@@ -1,10 +1,10 @@
-/*Coding Challenge for the week (weekend):
+/*********Coding Challenge for the week (weekend):
 There are n number of players for a game. (1 < n < 10000)
 There is a list that stores the score of each players e.g. [11, 22, 22, 33, 40, 45, 60, 60, 70]. In this case n = 9
 The rank of the players in this case is stored as [1,2,2,3,4,5,6,6,7]
 You are running the game and you need to report on the number of  players who have a score k or less. E.g. if k=2, your output should be 3.
 Write a program, that takes the list of scores and the highest selection score as the input and gives you the number of selected player as the output.
-*/
+***********/
 
 var count = 0;
 var score = '';
